980c518a7d9a8687bb4d29927831726e8a7d72ba
sandypratamaa <102770747+sandypratamaa@users.noreply.github.com>
2024-01-30 07:57:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import tensorflow as tf
 from PIL import Image
 from werkzeug.utils import secure_filename
 
-python -m pip install --upgrade pip
 pip install --no-build-isolation git+https://github.com/Throvn/pychalk@b78493cfc1797774b09ea0b4632f83e558efc448
 
 
