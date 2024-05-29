5779a1791524710fa28bd48e5a6c4612f51f1e22
sandypratamaa <102770747+sandypratamaa@users.noreply.github.com>
2024-05-29 22:52:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from PIL import Image
 from werkzeug.utils import secure_filename
 import base64
 
-# Set nilai default untuk hasil prediksi dan gambar yang diprediksi
+# Set default values for prediction result and predicted image
 hasil_prediksi = '(none)'
 gambar_prediksi = '(none)'
 
@@ -26,17 +26,17 @@ uploaded_file = st.sidebar.file_uploader("Choose an image...", type=["jpg", "jpe
 
 # Main content
 st.title("Welcome to Corn Disease Detection :corn:")
-st.markdown("*Aplikasi ini dapat membantu dalam mengklasifikasi kondisi tanaman jagung anda*")
+st.markdown("*This application can help classify the condition of your corn plants*")
 
-#add image
+# Add image
 # Define image size
 IMG_SIZE = (299, 299)
 st.image(image="ss.png", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 
-#penjelasan
-st.markdown("Aplikasi ini berguna untuk mendeteksi penyakit pada tanaman jagung menggunakan teknologi kecerdasan buatan (AI) dan algoritma deep learning untuk mendiagnosis penyakit pada tanaman jagung melalui gambar yang diunggah ke aplikasi ini. Dataset yang digunakan dalam sistem ini terdiri dari ribuan gambar tanaman jagung yang terinfeksi penyakit dan sehat. Saat pengguna mengunggah gambar tanaman jagung, sistem akan menganalisis gambar tersebut dan memberikan diagnosis. Algoritma deep learning digunakan karena dapat mempelajari fitur-fitur kompleks yang terkait dengan penyakit pada tanaman jagung dan menghasilkan diagnosis yang lebih akurat.")
+# Explanation
+st.markdown("This application is useful for detecting diseases in corn plants using artificial intelligence (AI) technology and deep learning algorithms to diagnose diseases in corn plants through images uploaded to this application. The dataset used in this system consists of thousands of images of corn plants infected with diseases and healthy ones. When users upload an image of a corn plant, the system will analyze the image and provide a diagnosis. Deep learning algorithms are used because they can learn complex features related to diseases in corn plants and produce more accurate diagnoses.")
 
-st.markdown(":corn: Terdapat 4 jenis kategori yang aplikasi dapat deteksi yaitu Corn Common Rust, Corn Northern Leaf Blight, Corn Gray Leaf Spot, dan Corn Healty yang akan di proses dibawah ini : ")
+st.markdown(":corn: There are 4 categories of diseases that the application can detect, namely Corn Common Rust, Corn Northern Leaf Blight, Corn Gray Leaf Spot, and Corn Healthy which will be processed below: ")
 
 if uploaded_file is not None:
     # Display the uploaded image
@@ -44,20 +44,26 @@ if uploaded_file is not None:
     st.write("")
     st.write("Classifying...")
 
-    # Predict
-  
-    test_image = Image.open(uploaded_file).resize(IMG_SIZE)
-    img_array = np.expand_dims(test_image, 0)
+    # Check if the uploaded image meets the expected dimensions
+    test_image = Image.open(uploaded_file)
+    if test_image.size != IMG_SIZE:
+        st.error("Uploaded image does not match the required dimensions. Please upload an image with dimensions (299, 299).")
+        hasil_prediksi = "Image does not match"
+    else:
+        # Predict
+        test_image = test_image.resize(IMG_SIZE)
+        img_array = np.expand_dims(test_image, 0)
 
-    predictions = model.predict(img_array)
-    hasil_prediksi = corndiseases_classes[np.argmax(predictions[0])]
+        predictions = model.predict(img_array)
+        hasil_prediksi = corndiseases_classes[np.argmax(predictions[0])]
 
     # Display result
     st.success(f"Prediction: {hasil_prediksi}")
 
-st.subheader(" Penjelasan mengenai jenis-jenis penyakit pada tanaman jagung ")
+st.subheader("Explanation of the types of diseases in corn plants")
 
-st.markdown("1.Corn Common Rust atau karat jagung adalah penyakit yang disebabkan oleh jamur Puccinia sorghi. Penyakit ini umum terjadi pada tanaman jagung di berbagai daerah dengan iklim yang hangat dan lembap. Gejalanya meliputi adanya bercak-bercak berwarna kuning atau oranye pada daun tanaman jagung. Infeksi karat jagung biasanya tidak menyebabkan kerusakan yang serius pada hasil panen, tetapi dapat mengurangi pertumbuhan dan produktivitas tanaman jika serangan parah terjadi.")
-st.markdown("2.Corn Gray Leaf Spot atau bercak daun abu-abu pada jagung disebabkan oleh jamur Cercospora zeae-maydis. Penyakit ini biasanya terjadi pada pertengahan hingga akhir musim tanam dan lebih umum terjadi di daerah yang lembap. Gejala utamanya adalah adanya bercak-bercak berwarna abu-abu atau coklat kehitaman pada daun jagung. Serangan berat dapat menyebabkan penurunan produksi dan kualitas jagung.")
-st.markdown("3.Corn Northern Leaf Blight atau bercak daun utara pada jagung disebabkan oleh jamur Exserohilum turcicum. Penyakit ini biasanya terjadi pada musim panas yang lembap dan hangat. Gejalanya meliputi adanya bercak-bercak berwarna coklat atau hijau keabu-abuan pada daun tanaman jagung. Serangan yang parah dapat menyebabkan kerusakan pada daun, mengurangi efisiensi fotosintesis, dan berpotensi mengurangi hasil panen.")
-st.markdown("4.Corn Healty adalah kondisi bahwa tanaman jagung anda dalam kondisi sehat.")
+st.markdown("1. Corn Common Rust is a disease caused by the fungus Puccinia sorghi. This disease commonly occurs on corn plants in various regions with warm and humid climates. Symptoms include yellow or orange spots on corn plant leaves. Common rust infection usually does not cause serious damage to crops, but it can reduce plant growth and productivity if severe infestations occur.")
+st.markdown("2. Corn Gray Leaf Spot, caused by the fungus Cercospora zeae-maydis. This disease usually occurs in mid to late growing season and is more common in humid areas. The main symptoms are gray or dark brown spots on corn leaves. Severe attacks can cause yield losses and decrease corn quality.")
+st.markdown("3. Corn Northern Leaf Blight, caused by the fungus Exserohilum turcicum. This disease usually occurs in warm and humid summers. Symptoms include brown or grayish-green spots on corn plant leaves. Severe attacks can cause leaf damage, reduce photosynthesis efficiency, and potentially reduce yields.")
+st.markdown("4. Corn Healthy indicates that your corn plants are in a healthy condition.")
+
