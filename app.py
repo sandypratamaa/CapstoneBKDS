18e202b6bc566a1a384003dafcb647d67eee7112
sandypratamaa <102770747+sandypratamaa@users.noreply.github.com>
2024-05-30 14:18:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ st.image(image="ss.png", caption=None, width=None, use_column_width=None, clamp=
 # Penjelasan
 st.markdown("Aplikasi ini berguna untuk mendeteksi penyakit pada tanaman jagung menggunakan teknologi kecerdasan buatan (AI) dan algoritma deep learning untuk mendiagnosis penyakit pada tanaman jagung melalui gambar yang diunggah ke aplikasi ini. Dataset yang digunakan dalam sistem ini terdiri dari ribuan gambar tanaman jagung yang terinfeksi penyakit dan sehat. Saat pengguna mengunggah gambar tanaman jagung, sistem akan menganalisis gambar tersebut dan memberikan diagnosis. Algoritma deep learning digunakan karena dapat mempelajari fitur-fitur kompleks yang terkait dengan penyakit pada tanaman jagung dan menghasilkan diagnosis yang lebih akurat.")
 
-st.markdown(":corn: Terdapat 4 jenis kategori yang aplikasi dapat deteksi yaitu Corn Common Rust, Corn Northern Leaf Blight, Corn Gray Leaf Spot, dan Corn Healty yang akan di proses dibawah ini: ")
+st.markdown(":corn: Terdapat 4 jenis kategori yang aplikasi dapat deteksi yaitu Corn Common Rust, Corn Northern Leaf Blight, Corn Gray Leaf Spot, dan Corn Healthy yang akan di proses dibawah ini: ")
 
 if uploaded_file is not None:
     # Display the uploaded image
@@ -44,7 +44,7 @@ if uploaded_file is not None:
     st.write("")
     st.write("Classifying...")
 
-# Predict
+    # Predict
     try:
         test_image = Image.open(uploaded_file).resize(IMG_SIZE)
         img_array = np.expand_dims(test_image, 0)
@@ -57,14 +57,14 @@ if uploaded_file is not None:
             st.success(f"Prediction: {hasil_prediksi}")
         else:
             st.error("Maaf tidak termasuk kategori")
-     except Exception as e:
+    except Exception as e:
         st.error(f"Error: {e}")
 else:
-   st.warning("Maaf ini bukan kategori jagung")
+    st.warning("Maaf ini bukan kategori jagung")
 
 st.subheader("Penjelasan mengenai jenis-jenis penyakit pada tanaman jagung")
 
 st.markdown("1. Corn Common Rust atau karat jagung adalah penyakit yang disebabkan oleh jamur Puccinia sorghi. Penyakit ini umum terjadi pada tanaman jagung di berbagai daerah dengan iklim yang hangat dan lembap. Gejalanya meliputi adanya bercak-bercak berwarna kuning atau oranye pada daun tanaman jagung. Infeksi karat jagung biasanya tidak menyebabkan kerusakan yang serius pada hasil panen, tetapi dapat mengurangi pertumbuhan dan produktivitas tanaman jika serangan parah terjadi.")
 st.markdown("2. Corn Gray Leaf Spot atau bercak daun abu-abu pada jagung disebabkan oleh jamur Cercospora zeae-maydis. Penyakit ini biasanya terjadi pada pertengahan hingga akhir musim tanam dan lebih umum terjadi di daerah yang lembap. Gejala utamanya adalah adanya bercak-bercak berwarna abu-abu atau coklat kehitaman pada daun jagung. Serangan berat dapat menyebabkan penurunan produksi dan kualitas jagung.")
 st.markdown("3. Corn Northern Leaf Blight atau bercak daun utara pada jagung disebabkan oleh jamur Exserohilum turcicum. Penyakit ini biasanya terjadi pada musim panas yang lembap dan hangat. Gejalanya meliputi adanya bercak-bercak berwarna coklat atau hijau keabu-abuan pada daun tanaman jagung. Serangan yang parah dapat menyebabkan kerusakan pada daun, mengurangi efisiensi fotosintesis, dan berpotensi mengurangi hasil panen.")
-st.markdown("4. Corn Healty adalah kondisi bahwa tanaman jagung anda dalam kondisi sehat.")
+st.markdown("4. Corn Healthy adalah kondisi bahwa tanaman jagung anda dalam kondisi sehat.")
